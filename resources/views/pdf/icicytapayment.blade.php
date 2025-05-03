b975c791c26585a520c94fcd5ece1c35c974b034
kahfialbhqi <128282234+kahfialbhqi@users.noreply.github.com>
2025-05-03 16:26:07 +0700

    Update icicytapayment.blade.php

--- resources/views/pdf/icicytapayment.blade.php
+++ resources/views/pdf/icicytapayment.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Payment Receipt</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ICICyTA 2024 Receipt</title>
     <style>
         * {
         margin: 0;
